bd6f30fc4d588ebd9c958c99d536ac58ff1795a1
rickyaward <rickyaward@gmail.com>
2015-05-01 12:24:51 -0400

    initial commit

---
+++ assets/javaScript/mypage.js
@@ -0,0 +1,17 @@
+var myemail = "rickyaward@gmail.com";
+    
+	document.getElementById("myemail").addEventListener("click", function() {
+    alert(myemail);
+     }, false);
+
+var phone = "407-687-2235";
+    
+	document.getElementById("phone").addEventListener("click", function() {
+    alert(phone);
+     }, false);
+
+var address = "3123 Anne Jolley CT Land O Lakes FL 34649";
+    
+	document.getElementById("address").addEventListener("click", function() {
+    alert(address);
+     }, false);
